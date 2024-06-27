d173151fdbb9f46af31814fa0dff0535e768be13
Arjee <arjeenunez@gmail.com>
2024-06-28 00:31:22 +0800

    Basic structure of color palette

---
+++ src/Palette.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+    render() {
+        console.log(this.props.palette)
+        return (
+            <div className="Palette">
+                {/* Navbar */}
+                <div className="Palette-colors">
+                    {/* color boxes */}
+                </div>
+                {/* footer */}
+            </div>
+        )
+    }
+}
+
+export default Palette;
