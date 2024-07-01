d65051b6282936a90639c15b4109af5fbb12459c
Arjee <arjeenunez@gmail.com>
2024-07-01 11:31:57 +0800

    Added react dom router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Palette from "./Palette";
 import Seedpalette from "./Seedpalette";
 import generatePalette from "./Colorhelper";
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      {/* <Palette {...Seedpalette[4]} /> */}
-      <Palette palette={generatePalette(Seedpalette[4])} />
+      <Routes>
+        <Route exact path="/" element={<h1>It workts - root</h1>} />
+        <Route exact path="/:id" element={ <h1>It works - individual route</h1>} />
+      </Routes>
+      {/* <Palette palette={generatePalette(Seedpalette[4])} /> */}
     </div>
   );
 }
