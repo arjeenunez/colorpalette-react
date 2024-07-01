007be98ce673007cbf1397b436d3caa96b314485
Arjee <arjeenunez@gmail.com>
2024-07-01 13:48:47 +0800

    Started styling palettes with MUI styled component

--- src/PaletteList.jsx
+++ src/PaletteList.jsx
@@ -1,17 +1,55 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { styled } from "@mui/material/styles"
+
+const Root = styled("div")({
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center"
+})
+
+const MyContainer = styled("div")({
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+})
+
+const MyNav = styled("nav")({
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white"
+})
+
+const Palettes = styled("div")({
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gap: "5%"
+})
 
 class PaletteList extends Component {
     render() {
         const { palettes } = this.props;
         return (
-            <div>
-                <h1>React Colors</h1>
-                {palettes.map(el => (
-                    <MiniPalette exact to={`/palette/${el.id}`} key={el.id} {...el} />
-                ))}
-            </div>
+            <Root>
+                <MyContainer>
+                    <MyNav>
+                        <h1>React Colors</h1>
+                    </MyNav>
+                    <Palettes>
+                        {palettes.map(el => (
+                            <MiniPalette exact to={`/palette/${el.id}`} key={el.id} {...el} />
+                        ))}
+                    </Palettes>
+                </MyContainer>
+            </Root>
         )
     }
 }
