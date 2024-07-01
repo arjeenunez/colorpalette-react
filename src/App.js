a356cd10665f32899a4f9de8196ebc315f823caf
Arjee <arjeenunez@gmail.com>
2024-07-01 11:51:34 +0800

    Fix palette routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Palette from "./Palette";
 import Seedpalette from "./Seedpalette";
 import generatePalette from "./Colorhelper";
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom"
 
 function App() {
+  function PaletteRoute() {
+    const { id } = useParams();
+    const [chosenPalette] = Seedpalette.filter(el => el.id === id);
+    return (
+      <Palette palette={generatePalette(chosenPalette)} />
+    )
+  }
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<h1>It workts - root</h1>} />
-        <Route exact path="/:id" element={ <h1>It works - individual route</h1>} />
+        <Route exact path="/:id" element={<PaletteRoute/>} />
       </Routes>
       {/* <Palette palette={generatePalette(Seedpalette[4])} /> */}
     </div>
